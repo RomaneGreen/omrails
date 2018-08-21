5e0293eccafeb85fd4b74b1c06fe93da3431e878
Romane <romane71193@gmail.com>
2018-08-20 23:16:54 -0400

    fix logout bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+  get "signin",to: 'devise/sessions#new'
+  get "signup" ,to: 'devise/registrations#new'
+  delete "signout" ,to: 'devise/sessions#destroy'
+  end
   get 'feed', to: 'feed#show'
 
   resources :users,only: :show,param: :username do
@@ -9,13 +16,7 @@ Rails.application.routes.draw do
   end
   resources :items
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-  get "signin",to: 'devise/sessions#new'
-  get "signup" ,to: 'devise/registrations#new'
-  delete "signout" ,to: 'devise/sessions#destroy'
-  end
+
   devise_for :views
   root 'pages#home'
   get 'about', to: 'pages#about'
